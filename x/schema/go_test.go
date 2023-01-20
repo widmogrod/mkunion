fadd091bf76e6f0856a23beec304ed62d8799161
widmogrod <widmogrod@gmail.com>
2023-01-20 23:14:53 +0100

    x/schema with support of relative paths

--- x/schema/go_test.go
+++ x/schema/go_test.go
@@ -250,9 +250,9 @@ func TestGoToSchema4(t *testing.T) {
 
 	result := ToGo(schema,
 		UnwrapStruct(BStruct{}, "BStruct"),
-		WhenPath([]string{"BStruct", "BaseStruct"}, UseStruct(&BaseStruct{})),
-		WhenPath([]string{"BStruct", "List", "[*]"}, UseStruct(AStruct{})),
-		WhenPath([]string{"BStruct", "Ma", "key"}, UseStruct(AStruct{})),
+		WhenPath([]string{"*", "BStruct", "BaseStruct"}, UseStruct(&BaseStruct{})),
+		WhenPath([]string{"*", "BStruct", "List", "[*]"}, UseStruct(AStruct{})),
+		WhenPath([]string{"*", "BStruct", "Ma", "key"}, UseStruct(AStruct{})),
 	)
 	assert.Equal(t, data, result)
 }
