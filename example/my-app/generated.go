31cb5c6aa3d1fbdc1fdbbc1a09326d9edc4ddec7
widmogrod <widmogrod@gmail.com>
2024-02-24 13:15:26 +0100

    my-app: remove generated.go

--- example/my-app/generated.go
+++
@@ -1,4 +0,0 @@
-package main
-
-//go:generate go run ../../cmd/mkunion/main.go shape-export --output=./src/workflow --input-go-file=../../x/workflow/workflow_machine.go
-// go:generate go run ../../cmd/mkunion/main.go shape-export --output=./src/workflow --input-go-file=../../x/schema/model.go
