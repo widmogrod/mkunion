884a8666d2dc247ec7c6c3786b02953bc8d93808
widmogrod <widmogrod@gmail.com>
2024-03-31 00:20:21 +0100

    dev: expose different port for documentation

--- dev/docs.sh
+++ dev/docs.sh
@@ -5,7 +5,8 @@ cwd=$(dirname "$0")
 project_root=$(dirname "$cwd")
 
 if [ "$1" == "run" ]; then
-  docker run --rm -it -p 8000:8000 -v ${project_root}:/docs squidfunk/mkdocs-material
+  echo "Serving documentation at http://localhost:8088"
+  docker run --rm -it -p 8088:8000 -v ${project_root}:/docs squidfunk/mkdocs-material
 elif [ "$1" == "build" ]; then
   docker run --rm -it -v ${project_root}:/docs squidfunk/mkdocs-material build
 else
