a7fb3ef34652a2fcda35fd91e3d2e51900dece29
widmogrod <widmogrod@gmail.com>
2024-06-15 22:54:13 +0200

    my-app: introduce timeout checking process for workflows with callbacks

--- example/my-app/background.go
+++ example/my-app/background.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"github.com/widmogrod/mkunion/x/storage/schemaless"
 	"github.com/widmogrod/mkunion/x/storage/schemaless/typedful"
@@ -13,6 +14,11 @@ import (
 func backgroundScheduled(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggregator[workflow.State, any]) (*taskqueue.FunctionProcessor[schemaless.Record[workflow.State]], *taskqueue.Description) {
 	procScheduled := &taskqueue.FunctionProcessor[schemaless.Record[workflow.State]]{
 		F: func(task taskqueue.Task[schemaless.Record[workflow.State]]) {
+			log := log.
+				WithField("op", "scheduled").
+				WithField("runID", task.Data.ID).
+				WithField("type", fmt.Sprintf("%T", task.Data.Data))
+
 			work := workflow.NewMachine(di, task.Data.Data)
 			err := work.Handle(context.TODO(), &workflow.Run{})
 			if err != nil {
@@ -21,7 +27,6 @@ func backgroundScheduled(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggr
 			}
 
 			newState := work.State()
-			//log.Infof("newState: %T", newState)
 
 			saving := []schemaless.Record[workflow.State]{
 				{
@@ -63,6 +68,8 @@ func backgroundScheduled(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggr
 					panic(err)
 				}
 			}
+
+			log.Infof("successful run")
 		},
 	}
 
@@ -77,7 +84,8 @@ func backgroundScheduled(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggr
 	descScheduled := &taskqueue.Description{
 		Change: []string{"create"},
 		Entity: "process",
-		Filter: `Data["workflow.Scheduled"].ExpectedRunTimestamp <= :now 
+		Filter: `Type == "process" 
+            AND  Data["workflow.Scheduled"].ExpectedRunTimestamp <= :now 
 			AND  Data["workflow.Scheduled"].ExpectedRunTimestamp > 0`,
 	}
 	return procScheduled, descScheduled
@@ -86,9 +94,19 @@ func backgroundScheduled(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggr
 func backgroundRetry(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggregator[workflow.State, any]) (*taskqueue.FunctionProcessor[schemaless.Record[workflow.State]], *taskqueue.Description) {
 	procRetry := &taskqueue.FunctionProcessor[schemaless.Record[workflow.State]]{
 		F: func(task taskqueue.Task[schemaless.Record[workflow.State]]) {
-			log := log.WithField("runID", task.Data.ID)
-			log.Infof("start retry")
+			log := log.
+				WithField("op", "retry").
+				WithField("runID", task.Data.ID).
+				WithField("type", fmt.Sprintf("%T", task.Data.Data))
 
+			switch sss := task.Data.Data.(type) {
+			case *workflow.Error:
+				log = log.
+					WithField("retries", sss.Retried).
+					WithField("version", task.Data.Version)
+			}
+
+			log.Infof("start retry")
 			work := workflow.NewMachine(di, task.Data.Data)
 			err := work.Handle(context.TODO(), &workflow.TryRecover{
 				RunID: task.Data.ID,
@@ -127,12 +145,71 @@ func backgroundRetry(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggregat
 	descRetry := &taskqueue.Description{
 		Change: []string{"create", "update"},
 		Entity: "process",
-		Filter: `Data["workflow.Error"].Retried < Data["workflow.Error"].BaseState.DefaultMaxRetries 
-			AND  Version < :now`,
-		//Filter: `After.Data["workflow.Error"].Retried < After.Data["workflow.Error"].BaseState.DefaultMaxRetries
-		//	AND  After.Version <> :now
-		//	AND  After.Type = "process"`,
+		Filter: `Type == "process" 
+			AND  Data["workflow.Error"].Retried < Data["workflow.Error"].BaseState.DefaultMaxRetries`,
+		//AND Data["workflow.Error"].Code != "async-timeout"`,
 	}
 
 	return procRetry, descRetry
+}
+
+func backgroundTimeout(di *workflow.DI, statesRepo *typedful.TypedRepoWithAggregator[workflow.State, any]) (*taskqueue.FunctionProcessor[schemaless.Record[workflow.State]], *taskqueue.Description) {
+	procTimeout := &taskqueue.FunctionProcessor[schemaless.Record[workflow.State]]{
+		F: func(task taskqueue.Task[schemaless.Record[workflow.State]]) {
+			log := log.
+				WithField("op", "timeout").
+				WithField("runID", task.Data.ID).
+				WithField("type", fmt.Sprintf("%T", task.Data.Data))
+
+			switch sss := task.Data.Data.(type) {
+			case *workflow.Await:
+				log = log.
+					WithField("expectedTimeout", sss.ExpectedTimeoutTimestamp).
+					WithField("version", task.Data.Version)
+			}
+
+			log.Infof("start timeout operation")
+			work := workflow.NewMachine(di, task.Data.Data)
+			err := work.Handle(context.TODO(), &workflow.ExpireAsync{
+				RunID: task.Data.ID,
+			})
+			if err != nil {
+				log.Errorf("err: %s", err)
+				return
+			}
+
+			newState := work.State()
+
+			_, err = statesRepo.UpdateRecords(schemaless.Save(schemaless.Record[workflow.State]{
+				ID:      task.Data.ID,
+				Data:    newState,
+				Type:    task.Data.Type,
+				Version: task.Data.Version,
+			}))
+			if err != nil {
+				if errors.Is(err, schemaless.ErrVersionConflict) {
+					// make it configurable, but by default we should
+					// just ignore conflicts, since that means we may have duplicate,
+					// or some other process already update it.
+					// assuming that queue is populated from stream of changes
+					// it such case (there was update) new message with new version
+					// will land in queue soon (if it pass selector)
+					log.Warnf("version conflict, ignoring: %s", err.Error())
+				} else {
+					log.Panicf("panic: %s", err)
+				}
+			}
+
+			log.Infof("successful expire")
+		},
+	}
+
+	descTimeout := &taskqueue.Description{
+		Change: []string{"create", "update"},
+		Entity: "process",
+		Filter: `Type == "process" 
+			AND  Data["workflow.Await"].ExpectedTimeoutTimestamp <= :now`,
+	}
+
+	return procTimeout, descTimeout
 }
