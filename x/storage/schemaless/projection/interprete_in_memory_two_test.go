c9c96593e9d1c10ec6183cca3449d18c8666095c
widmogrod <widmogrod@gmail.com>
2025-06-22 21:46:21 +0200

    refactor: enhance in-memory projection test with improved windowing logic and result validation
    Updated windowing configuration to use fixed windows with watermark-based triggers. Added result validation with `assert.ElementsMatch` to ensure expected outcomes.

--- x/storage/schemaless/projection/interprete_in_memory_two_test.go
+++ x/storage/schemaless/projection/interprete_in_memory_two_test.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"github.com/widmogrod/mkunion/x/schema"
-	"github.com/widmogrod/mkunion/x/shared"
 	"testing"
 	"time"
 )
@@ -19,46 +19,27 @@ func TestDefaultInMemoryInterpreter2(t *testing.T) {
 	})
 
 	dag := NewDAGBuilder()
-	loaded := dag.Load(&GenerateHandler{
-		Load: func(push func(message Item)) error {
-			for item := range GenerateItemsEvery(withTime(10, 0), 20, 10*time.Millisecond) {
-				push(item)
-			}
-			return nil
-		},
-	})
+	loaded := dag.
+		Load(&GenerateHandler{
+			Load: func(push func(message Item)) error {
+				for item := range GenerateItemsEvery(withTime(10, 0), 20, 10*time.Millisecond) {
+					push(item)
+				}
+				return nil
+			},
+		}).
+		Map(Log("loaded"))
 
 	mapped := loaded.
-		Window(WithTriggers(&AtPeriod{100 * time.Millisecond})).
-		Map(Log("window"))
+		Window(
+			WithFixedWindow(50*time.Millisecond),
+			WithTriggers(&AtWatermark{}),
+		).
+		Map(Log("window mapped"))
 
-	mapped.
+	end := mapped.
 		Map(&SimpleProcessHandler{
 			P: func(x Item, returning func(Item)) error {
-				d, _ := shared.JSONMarshal[schema.Schema](schema.FromGo(x))
-				log.Errorln("merge=", string(d))
-				//return MustMatchProcessItem(
-				//	x,
-				//	func(x *Item) error {
-				//		panic("implement me")
-				//	},
-				//	func(x *ItemAggregate) error {
-				//		panic("implement me")
-				//	},
-				//	func(x *ItemAggregateAndRetract) error {
-				//		returning(Item{
-				//			Key: x.Key,
-				//			Data: schema.FromGo(schema.Reduce(x.Aggregate, fmt.Sprintf("-1(%s)", schema.AsDefault[string](x.Retract, "-1")), func(x schema.Schema, agg string) string {
-				//				return fmt.Sprintf("%d,%s", schema.AsDefault[int](x, 0), agg)
-				//			})),
-				//			EventTime: x.EventTime,
-				//			Window:    x.Window,
-				//		})
-				//		return nil
-				//
-				//	},
-				//)
-
 				previous := schema.GetSchemaDefault[string](x.Data, "Previous", "")
 				current, _ := schema.GetSchema(x.Data, "Current")
 
@@ -76,9 +57,28 @@ func TestDefaultInMemoryInterpreter2(t *testing.T) {
 		}, WithAccumulatingAndRetracting()).
 		Map(Log("log"))
 
+	var result []string
+	end.
+		Map(&SimpleProcessHandler{
+			P: func(x Item, returning func(Item)) error {
+				result = append(result, schema.AsDefault[string](x.Data, "-"))
+				return nil
+			},
+		})
+
 	interpret := NewInMemoryTwoInterpreter()
 	err := interpret.Run(context.Background(), dag.Build())
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	expected := []string{
+		"9,8,7,6,5,",
+		"14,13,12,11,10,",
+		"19,18,17,16,15,",
+		"4,3,2,1,0,",
+	}
+
+	// order of elements is not guaranteed
+	assert.ElementsMatch(t, expected, result)
 }
