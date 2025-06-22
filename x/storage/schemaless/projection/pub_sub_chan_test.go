8d7a4723a8b69494623bf9fa09879f76b286bcc7
widmogrod <widmogrod@gmail.com>
2025-06-22 17:53:10 +0200

    refactor: improve TestPubSubChan synchronization to reduce flakiness
    Added `HasSubscribers` method to `PubSubChan` for safely checking active subscribers. Replaced arbitrary sleep with a proper wait mechanism during subscriber setup in tests, ensuring reliable synchronization.
    
    This resolves race conditions and reduces test flakiness.

--- x/storage/schemaless/projection/pub_sub_chan_test.go
+++ x/storage/schemaless/projection/pub_sub_chan_test.go
@@ -4,28 +4,30 @@ import (
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestPubSubChan(t *testing.T) {
 	psc := NewPubSubChan[string]()
 	go psc.Process()
 
-	var err = errors.New("foo")
+	var err = errors.New("foo err")
 
 	done := make(chan struct{})
 
-	started := make(chan struct{})
 	go func() {
 		defer close(done)
-		started <- struct{}{}
-		defer close(started)
 		err2 := psc.Subscribe(func(msg string) error {
 			assert.Equal(t, "foo", msg)
 			return err
 		})
 		assert.Error(t, err2, err)
 	}()
-	<-started
+
+	for !psc.HasSubscribers() {
+		// wait for subscribers
+		time.Sleep(time.Millisecond * 10)
+	}
 
 	err3 := psc.Publish("foo")
 	assert.NoError(t, err3)
